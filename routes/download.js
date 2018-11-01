225d27dcacdbccac9cc9660f4819f1a6df945869
Jonas Rosenqvist <jrosenqvist@w-jonros-pc-0.maxiv.lu.se>
2018-11-01 14:33:54 +0100

    test ui

--- routes/download.js
+++ routes/download.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 /* GET download  */
 router.get('/', function(req, res, next) {
-	const fs = require('fs');
-	var file = fs.readFileSync("files/" + req.query.file, "binary");
+  const fs = require('fs');
+  try{
+    var file = fs.readFileSync("files/" + req.query.file, "binary");
+  }catch(error){
+    throw new Error("404 - File not found");
+  }
+	
 
   res.setHeader('Content-Length', file.length);
   res.setHeader('content-disposition', 'attachment; filename=' + req.query.file);
