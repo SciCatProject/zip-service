997de1760c5d3cb8c9842bee4d1e4d03b247903a
Jonas Rosenqvist <jrosenqvist@w-jonros-pc-0.maxiv.lu.se>
2018-11-13 10:32:55 +0100

    ui

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,19 +6,21 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var zipRouter = require('./routes/zip');
 var downloadRouter = require('./routes/download');
+const bodyParser = require('body-parser');
+const session =  require('express-session');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({ secret: 'fj9832mnsaf3j9adsa', resave: true, saveUninitialized: true, }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 app.use('/zip', zipRouter);
 app.use('/download', downloadRouter);
@@ -37,7 +38,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {msg: "catch-all 500 error"});
 });
 
 module.exports = app;
--- routes/zip.js
+++ routes/zip.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const ZIP_FILES_PATH = require('../constants');
+var redis = require('redis');
+var client = redis.createClient();
+
 router.get('/', function(req, res, next) {
 	console.log("GET /zip");
 	res.statusCode = 400;
@@ -11,13 +14,12 @@ router.get('/', function(req, res, next) {
 
 /* POST zip */
 router.post('/', function(req, res, next){
-	const json = req.body;
+	const json = JSON.parse(req.body.data);
 	console.log("POST /zip");
 	console.log(json);
 	const path = json["base"];
 	if (!path || path.length === 0){
-		res.statusCode = 400;
-		res.send("JSON missing 'base'");
+		res.render("error", {msg: "JSON missing 'base'"})
 		return;
 	}
 	const files = json["files"];
@@ -33,16 +35,23 @@ router.post('/', function(req, res, next){
 		return;
 	}
 	try{
-		const zipFile = zipFiles(path, files, res);
+		var sessData = req.session;
+		sessData.total = files.length;
+		sessData.current = 0;
+		sessData.zipFile = '';
+		var id = sessData.id;
+		client.set(id, "");
+		zipFiles(path, files, req, res, id);
 	}catch(error){
 		res.statusCode = 500;
 		res.send("The files could not be zipped");
 		return;
 	}
+
 });
 module.exports = router;
 
-const zipFiles = (path, files, res) => {
+const zipFiles = (path, files, req, res, id) => {
 	const zipFile = require('crypto').createHash('md5').update(path).digest("hex") + "_" + new Date().getTime() + ".zip";
 	const archiver = require('archiver');
 	const fileStream = fs.createWriteStream(ZIP_FILES_PATH + zipFile);
@@ -55,10 +64,29 @@ const zipFiles = (path, files, res) => {
 		throw err;
 	});
 	fileStream.on('close', function() {
-		res.send("/download?file=" + zipFile);
+		client.set(id, "/download?file=" + zipFile);
+		req.session.zipFile= "/download?file=" + zipFile;
 	});
 	archive.pipe(fileStream);
-	files.map(file => fs.existsSync(path + "/" + file) ? archive.file(path + "/" + file, { name: file }) : null);
+	files.map(file => zipSingleFile(path, file, req, archive));
 	archive.finalize();
+	res.render("zipping");
 
 }
+const zipSingleFile = (path, file, req, archive)  => {
+	
+		fs.existsSync(path + "/" + file) ? archive.file(path + "/" + file, { name: file }) : null;
+		req.session.current = req.session.current + 1;
+	 
+	
+}
+router.get("/polling", function(req, res, next){
+	client.get(req.session.id, function(error, result) {
+		if (error) throw error;
+		if (result && result !== ""){
+			res.send(result);
+		}else{
+			res.send(req.session.current + "");
+		}
+	  });
+});
