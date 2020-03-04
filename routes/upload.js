7f7e7d39b7c437b6779b8447772079298ba34571
Henrik Johansson <henrik.johansson712@gmail.com>
2020-03-04 11:47:17 +0100

    Added check to avoid overwriting files

--- routes/upload.js
+++ routes/upload.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
+
 const fs = require("fs");
-const path = require("path");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 const StreamZip = require("node-stream-zip");
 
 const config = require("../local.config.json");
+
 const homePath = "/nfs/groups/beamlines/dram";
 
 router.post("/", function(req, res) {
-  console.log("req", req);
+  //   console.log("req", req);
   const data = req.body;
 
   if (!config.jwtSecret) {
@@ -28,13 +30,18 @@ router.post("/", function(req, res) {
 
   jwt.verify(jwtValue, config.jwtSecret, (err, decoded) => {
     if (err) {
-      return res.status(400).send("Invalid JSON web token\n");
+      return res.status(403).send("Invalid JSON web token\n");
     }
 
     console.log("decoded jwt", decoded);
 
     let saveDir;
     if (data["dir"]) {
+      if (data["dir"].indexOf("..") > -1 || data["dir"].indexOf("/") > -1) {
+        return res
+          .status(403)
+          .send(`Directory names containing '..' or '/' are not allowed`);
+      }
       saveDir = data["dir"];
     } else {
       saveDir = generateDirName();
@@ -64,21 +71,42 @@ router.post("/", function(req, res) {
       return res.status(500).send("Unable to read zip file\n");
     });
 
-    zip.on("ready", () => {
-      console.log("Entries read: " + zip.entriesCount);
-      for (const entry of Object.values(zip.entries())) {
-        const desc = entry.isDirectory ? "directory" : `${entry.size} bytes`;
-        console.log(`Entry ${entry.name}: ${desc}`);
-      }
-      zip.close();
-    });
+    let entries;
 
-    res.send("File uploaded\n");
+    zip.on("ready", () => {
+      const saveDir = fs.readdirSync(savePath);
+      entries = Object.keys(zip.entries());
+      console.log("entries", entries);
+      const duplicates = entries.filter(entry => saveDir.includes(entry));
+      console.log("duplicates", duplicates);
+      if (duplicates.length > 0) {
+        zip.close();
+        return res
+          .status(500)
+          .send(
+            `The file(s) ${duplicates} already exists in the chosen directory. Please choose another directory or rename the conflicting file(s).`
+          );
+      } else {
+        zip.extract(null, savePath, (err, count) => {
+          if (err) {
+            return res.status(500).send("Unable to extraxt files\n");
+          }
+          console.log(`Extracted ${count} files`);
+          zip.close();
+
+          return res.status(200).send({
+            message: "Upload successful",
+            files: entries,
+            location: savePath
+          });
+        });
+      }
+    });
   });
 });
 
 function generateDirName() {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const characters = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
   const charactersLength = characters.length;
   let result = "";
   for (let i = 0; i < 6; i++) {
